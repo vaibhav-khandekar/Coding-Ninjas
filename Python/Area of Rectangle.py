e4ca2effada16fc1403a1f1d36db7e8ecec139f7
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2023-10-12 14:49:01 +0530

    Create Area of Rectangle.py

---
+++ Python/Area of Rectangle.py
@@ -0,0 +1,17 @@
+#Your code goes here
+
+# Solution 1
+def Area(l,b):
+    return l*b
+
+l,b = input().split()
+l = int(l)
+b = int(b)
+print(Area(l,b))
+
+# Solution 2
+
+l,b = input().split()
+l = int(l)
+b = int(b)
+print(l*b)
