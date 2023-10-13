4255498369dbad71b4210401f31f051fcb42b608
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2023-10-13 11:17:32 +0530

    Create Area of Rectangle.java

---
+++ Java/Area of Rectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(l*b);
+	}
+}
